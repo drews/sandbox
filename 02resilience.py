ba13565c3b33245c91287e0f6e0334db438e9948
Drew Shapiro <drew.shapiro@gmail.com>
2025-10-02 16:37:51 +0000

    refactor variable names for clarity and consistency in resilience demonstration

--- 02resilience.py
+++ 02resilience.py
@@ -1,20 +1,24 @@
 # demonstrate resilience (i.e. recovery from "disaster")
+import sys
 
 # setup some basic variables and inspect their types
-number_as_string = "3"
-print(type(number_as_string))
-number_as_int = 3
-print(type(number_as_int))
 
-non_number_as_string = "hello world"
-print(int(number_as_string)) # will work
+a = "3"
+print(type(a)) # <type str>
+print(int(a)) # will work
+
+b = 3
+print(type(b))
+
+c = "hello world"
 
 some_number = -1 # sentinel value
 try:
     # 1. have a disaster
-    some_number = int(non_number_as_string) # won't work
+    some_number = int(c) # won't work
 except:
     # 2. recover from error
     print(repr(sys.exception()))
 if some_number < 0:
     print("some_number is negative and thus int-parsing might've failed")
+
